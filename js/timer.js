39993e6bc18b68143af5292d276006fa88a9f50a
Hayoun Lee <sidhlee@gmail.com>
2021-02-19 19:59:57 -0500

    fix: the bug caused by menu opened during fail sequence

--- js/timer.js
+++ js/timer.js
@@ -7,25 +7,44 @@ import { failQuestion, setNextQuestion } from './quiz.js';
 
 let timer;
 
+// TODO: make this easier to understand
 export function startTimer() {
+  let failed = false;
   if (timer !== undefined) return;
-  const showAndUpdateTime = async () => {
-    if (state.timeRemaining < 0) {
-      await failQuestion();
-      setNextQuestion();
+  const timeoutCallback = async () => {
+    if (state.timeRemaining < 0 && !state.isMenuOpen) {
+      // avoid failing more than once
+      if (!failed) {
+        await failQuestion();
+        failed = true;
+      }
+      // don't start next question while menu is open
+      if (!state.isMenuOpen) {
+        setNextQuestion();
+        return;
+      } else {
+        // check again in 1 second if menu is open
+        timer = setTimeout(timeoutCallback, 1000);
+        return;
+      }
+    } // callback in 1 sec if menu is open and time is up
+    else if (state.timeRemaining < 0 && state.isMenuOpen) {
+      timer = setTimeout(timeoutCallback, 1000);
       return;
     } else {
+      // if still counting
       $('.hud__time > span').text(state.timeRemaining);
       setState({
         clearTime: state.clearTime + 1,
         timeRemaining: state.timeRemaining - 1,
       });
 
-      timer = setTimeout(showAndUpdateTime, 1000);
+      timer = setTimeout(timeoutCallback, 1000);
+      return;
     }
   };
 
-  showAndUpdateTime();
+  timeoutCallback();
 }
 
 export function clearTimer() {
