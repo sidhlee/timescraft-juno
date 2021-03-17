4917ad6d5b6d496880a9080d6429fe315446b6ac
Hayoun Lee <sidhlee@gmail.com>
2021-03-17 16:08:22 -0400

    chore: clean up

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 import App from './js/app.js';
 
 $(function () {
-  // PWA worker registration
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../sw.js').then(() => {
-        console.log('Service Worker Registered');
-      });
-    });
-  }
-
   App.init();
 });
-
-//TODO: add stats menu
-//TODO: fix sound coming out late on android
